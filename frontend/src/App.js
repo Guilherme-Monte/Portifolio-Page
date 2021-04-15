e8e390c8879389ea896ec1c13158a674ee6785e1
Guilherme-Monte <guilhermemonte.dev@gmail.com>
2021-04-15 12:06:20 -0300

    readme update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button, Container, Title, TomatoButton, Wrapper, ReversedButton } from "./styles";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Portifolio
+      <Wrapper>
+        <Title>
+          Hello World
+        </Title>
+        <Container>
+          <TomatoButton as={ReversedButton}>Portifolio 1</TomatoButton>
+          <Button primary as="a" href="/">Primary</Button>
+        </Container>
+      </Wrapper>
     </div>
   );
 }
